98cc25f4e685a4cb717e91100a721f92292e0565
JonyPeixoto <jonathanppeixoto@gmail.com>
2020-10-29 20:27:19 -0300

    update hello-world-and-variables

---
+++ C/Variables/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int ano_nascimento = 1996;
+    int ano_atual = 2020;
+
+    int resultado = (ano_atual - ano_nascimento);
+
+    printf("Jonathan is %d years old",resultado);
+
+    return 0;
+}
